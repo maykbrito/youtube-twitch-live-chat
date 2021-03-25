32cad126c56a06be225a9bc6c4f5bb63ec5a89bb
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-03-24 22:38:26 -0300

    Separando mensagens de perguntas

--- twChat.js
+++ twChat.js
@@ -15,17 +15,28 @@ const content = (tags, message, channel) => `
 const messages = []
 const questions = []
 
-const printMessage = (tag, message, channel) => {
-  div.innerHTML = content(tag, message, channel) + div.innerHTML;
+const printMessage = ({ tags, message, channel }) => {
+  div.innerHTML = content(tags, message, channel) + div.innerHTML;
   document.body.scrollTo({ top: 600 })
 }
 
+const isQuestion = message => message.includes("#pergunta") ? true : false
+const saveQuestion = ({ tags, message, channel }) => questions.unshift({ channel, tags, message })
+const printQuestionInConsoleLog = questions => questions.forEach(
+  question =>
+    console.log(`${question.channel} - ${question.tags['display-name']} - ${question.message}`)
+)
+
 client.on('message', (channel, tags, message, self) => {
-  printMessage(tags, message, channel)
-  messages.unshift({ channel, tags, message })
+  const messageData = { tags, message, channel }
+
   if (message.includes("#pergunta")) {
     console.clear()
-    questions.unshift({ channel, tags, message })
-    questions.forEach(question => console.log(`${question.channel} - ${question.tags['display-name']} - ${question.message}`))
+    saveQuestion(messageData)
+    printQuestionInConsoleLog(questions)
+  } else {
+    printMessage(messageData)
+    messages.unshift(messageData)
   }
+
 })
